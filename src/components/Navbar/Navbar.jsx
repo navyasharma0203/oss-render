82c92dfb3caf9ddfc00100fda3e7f5042d98ea4c
Jagruti <jagrutirkm@gmail.com>
2023-03-01 01:58:00 +0530

    login implementation

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+
+import LoginButton from "./LoginButton";
+
+const Navbar = () => {
+  return (
+    <nav className="w-full h-16 bg-gray-200 p-2 flex items-center justify-center">
+      <LoginButton />
+    </nav>
+  )
+}
+
+export default Navbar;
---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect, createContext } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../../firebase/firebase.js';
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const subscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    })
+
+    return () => subscribe();
+  }, [])
+
+  return (
+    <AuthContext.Provider value={{ user }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
