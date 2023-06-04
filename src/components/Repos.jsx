d7e66bc509025bbde6f9e279cf222c23c705ccc7
Jagruti <jagrutirkm@gmail.com>
2023-06-05 01:13:23 +0530

    tags update

--- src/components/Repos.jsx
+++ src/components/Repos.jsx
@@ -6,28 +6,31 @@ import { collection, addDoc } from "firebase/firestore";
 import { useAuthState } from "react-firebase-hooks/auth";
 
 function Repos({ posts, name, tags, desc }) {
-    const [user] = useAuthState(auth);
+  const [user] = useAuthState(auth);
 
-  const handleSave = async (name, desc, link, tag) => {
+  const handleSave = async (name, desc, link, tags,upforgrabs__link ) => {
     try {
       const docRef = await addDoc(collection(db, "mySaves"), {
         userId: user?.email,
         name: name,
         desc: desc,
         link: link,
+        tags: tags,
+        upforgrabs__link : upforgrabs__link ,
+        
       });
       console.log("Saved successfully");
-      alert("Saved Successfully");
+      alert("Saved Successfully!");
     } catch (e) {
       console.log(e);
-      alert("Could not save.");
+      alert("Server error : Could not save");
     }
   };
 
   return (
     <>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 max-w-7xl items-center mx-auto gap-12">
-        {posts.map(({ name, desc, link, tag }, index) => (
+        {posts.map(({ name, desc, link, tags, upforgrabs__link  }, index) => (
           <div
             className="w-full rounded-lg shadow-md lg:max-w-sm m-4 grid gap-8 p-4 h-full"
             key={index}
@@ -36,13 +39,9 @@ function Repos({ posts, name, tags, desc }) {
 
             <p className="mb-2 leading-normal">{desc}</p>
 
-            {/* <div className='flex overflow-hidden w-full flex-wrap items-center'>
-                                    {tagsValue.split('\n').map((tag, index) => (
-                                        <div className="text-sm text-blue-500 bg-blue-100 rounded shadow px-2 py-1 m-1" key={index}>
-                                        {tag.trim()}
-                                        </div>
-                                    ))}
-                                    </div> */}
+            <p className="leading-normal text-sm text-blue-500 bg-blue-100 rounded shadow px-3 py-1 w-1/3 h-7 ">
+              {tags}
+            </p>
 
             <div className="extra-btn">
               <div className="small-btn">
@@ -50,22 +49,26 @@ function Repos({ posts, name, tags, desc }) {
                   <a className=" sbtn hover:cursor-pointer">
                     <div className="s-btn">
                       <BsFillBookmarkFill
-                        className="text-gray-500 hover:text-green-700  text-2xl"
+                        className="text-gray-500 hover:text-green-600  text-2xl"
                         onClick={() => {
-                          handleSave(name, desc, link, tag);
+                          handleSave(name, desc, link, tags,upforgrabs__link);
                         }}
                       />
                     </div>
                   </a>
                 </div>
 
-                {/* <div className=" flex justify-end items-center">
+                <div className=" flex justify-end items-center">
                   <a className=" ibtn hover:cursor-pointer">
-                    <div className="i-btn">
-                      <AiOutlineInfoCircle className="text-indigo-500 hover:text-lime-800 text-2xl ml-2" />
-                    </div>
+                    <p>
+                      <a href={upforgrabs__link}>
+                        <div className="i-btn">
+                          <AiOutlineInfoCircle className="text-indigo-500 hover:text-green-700 text-2xl ml-2" />
+                        </div>
+                      </a>
+                    </p>
                   </a>
-                </div> */}
+                </div>
               </div>
 
               <div className="rd-btn flex justify-end items-center">
